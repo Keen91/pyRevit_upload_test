23e656971fbae542f6f2e6d54de78d5fd828cd9d
Kinan Hatahet <kinan.hatahet@gmail.com>
2022-12-09 15:03:22 +0300

    Added files

---
+++ EF-Tutorials.tab/Dev.panel/col2.stack/HelloWorld 4.pushbutton/script.py
@@ -0,0 +1,6 @@
+__title__   = "Hello BIM World 4!"
+__author__  = "Kinan"
+__doc__     = """This is Hello World Button. Click on it to see what happens..."""
+
+if __name__ == '__main__':
+    print("Hello BIM World 4")
